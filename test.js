fc9e13c7551d1ae8ae3803a93e7397b7f50b3355
loganpowell <loganp@tepper.cmu.edu>
2018-05-07 16:36:30 -0400

    Whoot! Create /tmp folder in C:/ drive prior and include `fs` node native lib

--- test.js
+++ test.js
@@ -1,5 +1,15 @@
 var getCountyGeoJSON = require('./index').getCountyGeoJSON;
+var fs = require('fs');
 
-getCountyGeoJSON('500k', ['IA'], function(err, data) {
-  console.log(JSON.stringify(data));
+var test = getCountyGeoJSON('500k', ['IA'], function(err, data) {
+  // console.log(JSON.stringify(data));
+  var result = JSON.stringify(data)
+  fs.writeFile("/tmp/test.geojson", result, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+
+      console.log("The file was saved!");
+  });
+
 });
